6eb37792efa6ce474e1be12c3cb4c63a2ec6cc76
kapinjal <kapinjal@webmobsoft.com>
2020-08-30 20:12:29 +0530

    add remove data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    selectValue: "",
+    baseRate: "",
+    tableData: []
+  }
+
+  handleDropdownChange = (e) => {
+    this.setState({ selectValue : e.target.value });
+  }
+
+  updateRateInput = (key, value) => {
+    this.setState({ [key]: value });
+  }
+
+  submitForm = async (e) => {
+    e.preventDefault();
+    const row = { select : this.state.selectValue, baseRate : this.state.baseRate };
+    await this.setState({
+        tableData : [...this.state.tableData, row]
+    })
+  }
+
+  deleteRateItem = async (id) => {
+    let tableData = [...this.state.tableData];
+    tableData.splice(id, 1);
+    await this.setState({ tableData }); 
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.submitForm}>
+          <select className="form-class" onChange={this.handleDropdownChange}>
+            <option defaultValue="">Skype</option>
+            <option value="Java">Java</option>
+            <option value="NodeJs">NodeJs</option>
+            <option value="HTML">HTML</option>
+            <option value="PHP">PHP</option>
+          </select>
+          <input type="text" placeholder="$20" className="form-class" value={this.state.baseRate} onChange={(e) => this.updateRateInput("baseRate", e.target.value)} />
+          <button type="submit" className="form-class">Add</button>
+        </form>
+        <table className="table-class">
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Skill</th>
+              <th>Base Hourly Rate(USD)</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.tableData.map((item, index) => {
+              return ( 
+                <tr key={index}>
+                  <td>{index+1}</td>
+                  <td>{item.select}</td>
+                  <td>${item.baseRate}</td>
+                  <td><button onClick={() => this.deleteRateItem(index)}>Delete</button></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App;
